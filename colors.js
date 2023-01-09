5edf66c506c6559225be18e41c88620f2064673b
baejihun <122069364+baejihun@users.noreply.github.com>
2023-01-09 12:01:12 +0900

    upload

---
+++ colors.js
@@ -0,0 +1,27 @@
+var Links = {
+ setcolor:function (color){
+    $('a').css('color', color);
+ }   
+}
+var Body = {
+ setColor:function (color){
+    $('body').css('color', color)
+ },
+ setBackgroundColor:function (color){
+    $('body').css('backgroundColor', color)
+ }
+}
+function nightDayHandler(self){
+    var target = document.querySelector('body');
+    if(self.value === 'night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day';
+        Links.setcolor('powderblue');
+}   else {   
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value = 'night';
+        Links.setcolor('blue');
+    }
+}   
